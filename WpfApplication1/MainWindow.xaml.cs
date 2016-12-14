1ec91a7e95a36c675298db1324d1cba3ae8c8f97
Antoine GIRARD <sapk@sapk.fr>
2016-12-14 14:24:34 +0100

    Init

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            pwdTextBox.Text = System.IO.Directory.GetCurrentDirectory();
+        }
+
+        private void refreshClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void startBallonBtnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("Ballon.exe");
+        }
+
+        private void startPremierBtnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("Premier.exe");
+        }
+    }
+}
